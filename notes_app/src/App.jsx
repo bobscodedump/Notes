9e04dd1c50ec95f5b6df47b861c008a4ecb5e188
Lee Seng Kitt <87774484+bobscodedump@users.noreply.github.com>
2023-01-31 23:48:42 +0800

    before submit button

--- notes_app/src/App.jsx
+++ notes_app/src/App.jsx
@@ -4,6 +4,7 @@ import ModalComponent from "./components/ModalComponent";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  // const [noteData, setNoteData] = useState({});
 
   return (
     <div className="min-h-screen flex flex-col justify-start bg-waikawa-gray-700">
--- notes_app/src/components/ModalComponent.jsx
+++ notes_app/src/components/ModalComponent.jsx
@@ -8,6 +8,15 @@ function ModalComponent({ open, onClose }) {
     note: "",
   });
 
+  const { description, note } = inputs;
+
+  const onChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="overlay fixed w-full h-full bg-waikawa-gray-50 bg-opacity-50 flex justify-center pt-12">
       <div className="modalContainer bg-waikawa-gray-800 max-w-4xl w-full fixed max-h-5xl h-1/2 m-auto shadow-2xl mt-12">
@@ -21,16 +30,30 @@ function ModalComponent({ open, onClose }) {
             Description:
           </h1>
           <form>
-            <input
-              type="text"
+            <textarea
               name="description"
+              rows="1"
               placeholder="Enter Description"
-              className="mt-1.5 ml-3"
+              className="mt-2 ml-3 rounded-sm bg-waikawa-gray-100"
               value={description}
+              onChange={(e) => onChange(e)}
             />
           </form>
         </section>
-        <div className="modalForm"></div>
+        <section className="note grid justify-center text-center mt-5">
+          <h1 className="text-waikawa-gray-100 font-sans text-3xl">Note:</h1>
+          <form>
+            <textarea
+              name="note"
+              cols="70"
+              rows="10"
+              placeholder="Enter Note"
+              className="mt-1.5 ml-3 rounded-sm bg-waikawa-gray-100"
+              value={note}
+              onChange={(e) => onChange(e)}
+            />
+          </form>
+        </section>
       </div>
     </div>
   );
